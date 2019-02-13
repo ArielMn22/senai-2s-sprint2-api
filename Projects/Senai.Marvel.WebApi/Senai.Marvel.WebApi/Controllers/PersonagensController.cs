f7364ac343faa445884f859305bb847e0c43fcc8
Ariel Paixão Dos Santos <33747821839@info.local>
2019-02-13 17:23:42 -0200

    Subindo anotações e projetos, Svigufo e personagensMarvel

---
+++ Projects/Senai.Marvel.WebApi/Senai.Marvel.WebApi/Controllers/PersonagensController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Marvel.WebApi.Domains;
+
+namespace Senai.Marvel.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonagensController : ControllerBase
+    {
+        List<PersonagemDomain> personagensMarvel = new List<PersonagemDomain>()
+        {
+            new PersonagemDomain {Id = 1, Nome = "Homem-Aranha", Lancamento = "Amazing Fantasy #15 (1962)", Descricao = "O adolescente que é mordido por uma aranha radioativa conseguiu se tornar no super-herói mais famoso de todos os tempos, estabelecendo uma forte conexão com todos os jovens que entram em contato com sua história."},
+            new PersonagemDomain {Id = 2, Nome = "O Incrível Hulk", Lancamento = "O Incrível Hulk #1 (1962)", Descricao = "O Incrível Hulk não é mais do que uma alegoria sobre a perda do auto-controle e como a raiva pode ser canalizada para algo positivo."},
+            new PersonagemDomain {Id = 3, Nome = "Homem de Ferro", Lancamento = "Tales of Suspense #39 (1963)", Descricao = "Homem de Ferro é um personagem conhecido por ter pegado em uma situação extremamente má e se tornar em um super-herói. Com a ajuda de dinheiro e genialidade, claro."}
+        };
+        
+        [HttpGet]
+        public IEnumerable<PersonagemDomain> Get()
+        {
+            return personagensMarvel;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            PersonagemDomain personagemMarvel = personagensMarvel.Find(x => x.Id == id);
+
+            if(personagemMarvel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(personagemMarvel);
+        }
+    }
+}
---
+++ Projects/Senai.SviGufo.WebApi/Senai.SviGufo.WebApi/Controllers/TiposEventosController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.SviGufo.WebApi.Domains;
+
+namespace Senai.SviGufo.WebApi.Controllers
+{
+    [Produces("application/json")] // Diz que todos os métodos retornaram um arquivo .json
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class TiposEventosController : ControllerBase
+    {
+        //Declaração de um objeto List do tipo TipoEventoDomain
+        List<TipoEventoDomain> tiposEventos = new List<TipoEventoDomain>()
+        {
+            new TipoEventoDomain{ Id = 1, Nome = "Tecnologia"},
+            new TipoEventoDomain{ Id = 2, Nome = "Arquitetura"}
+        };
+
+        /// <summary>
+        /// Retorna uma string através do método Get()
+        /// </summary>
+        /// <returns>String</returns>
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    return "Requisição recebida com sucesso!";
+        //}
+
+        [HttpGet]
+        public IEnumerable<TipoEventoDomain> Get()
+        {
+            return tiposEventos;
+        }
+
+        /// <summary>
+        /// Retorna um tipo de evento pelo seu Id
+        /// </summary>
+        /// <param name="id">id do evento</param>
+        /// <returns>TipoEventoDomain</returns>
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //Busca um tipo de evento pelo seu id
+            TipoEventoDomain tipoEventoSerProcurado = tiposEventos.Find(x => x.Id == id);
+
+            if (tipoEventoSerProcurado == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tipoEventoSerProcurado);
+        }
+    }
+}
